21bb3b3974bfa8d2be39a3aa3c3f38652cae572b
Gonéri Le Bouder <goneri@lebouder.net>
2019-05-26 20:54:02 -0400

    build 8.0 and 7.2 images

--- build-all.sh
+++ build-all.sh
@@ -1,36 +1,64 @@
 #!/bin/sh
 set -eux
 
-MNT=new
-FILE=new.img
+MNT=/root/new
 VND=vnd0
+VERSION="8.0 7.2"
 
-mkdir new
-dd if=/dev/zero of=${FILE} bs=4096 count=1015200 progress=62000
-vnconfig ${VND} ${FILE}
+for version in $VERSION; do
+file="netbsd-${version}.raw"
+mkdir -p ${MNT}
+dd if=/dev/zero of=${file} bs=4096 count=1000000 progress=62000
+vnconfig ${VND} ${file}
 gpt create ${VND}
 gpt add -a 4k -l swap -s 1G -t swap ${VND}
 gpt add -a 4k -s 2G -l root -t ffs ${VND}
 gpt show ${VND}
 
 dkctl ${VND} makewedges
+dk_dev=$(dkctl ${VND} listwedges|grep ffs|sed 's,:.*,,')
 # newfs dos and ffs
 sleep 2
-newfs -O 2 -n 500000 -b 4096 /dev/rdk1
-mount /dev/dk1 ${MNT}
+newfs -O 2 -n 500000 -b 4096 /dev/r${dk_dev}
+mount /dev/${dk_dev} ${MNT}
 
 for i in base.tgz etc.tgz kern-GENERIC.tgz; do
-    curl -L http://ftp.fr.netbsd.org/pub/NetBSD/NetBSD-8.0/x86_64/binary/sets/${i} | tar xfz - -C new
+    curl -L http://ftp.fr.netbsd.org/pub/NetBSD/NetBSD-${version}/amd64/binary/sets/${i} | tar xfz - -C ${MNT}
 done
 echo 'rc_configured=YES
-sshd="YES"
-resize_disklabel=YES
-resize_disklabel_disk=ld0
-resize_disklabel_part=a
-resize_root=YES
-resize_root_flags="-p"
-resize_root_postcmd="/sbin/reboot -n"
-' >> new/etc/rc.conf
+sshd=YES
+dhcpcd=YES
+grow_root_fs=YES
+' >> $MNT/etc/rc.conf
+
+echo '#!/bin/sh
+#
+# PROVIDE: grow_root_fs
+# BEFORE:  fsck_root
+
+$_rc_subr_loaded . /etc/rc.subr
+
+name="grow_root_fs"
+rcvar=$name
+start_cmd="grow_root_fs_start"
+stop_cmd=":"
+
+grow_root_fs_start()
+{
+gpt resizedisk ld0
+gpt resize -i 2 ld0 && reboot
+if resize_ffs -c /dev/r$(sysctl -r kern.root_device); then
+    resize_ffs -p -y -v /dev/r$(sysctl -r kern.root_device) && reboot -n
+else
+    sed -i "s,grow_root_fs=.*,# grow_root_fs=NO  # Auto-disabled," /etc/rc.conf
+fi
+}
+
+load_rc_config $name
+run_rc_command "$1"
+' > $MNT/etc/rc.d/grow_root_fs
+chmod +x $MNT/etc/rc.d/grow_root_fs
+
 
 echo '
 NAME=root	/		ffs	rw	1 1
@@ -40,22 +68,25 @@ ptyfs		/dev/pts	ptyfs	rw
 procfs /proc procfs rw
 /dev/cd0a               /cdrom  cd9660  ro,noauto
 tmpfs           /var/shm        tmpfs   rw,-m1777,-sram%25
-' > new/etc/fstab
-cp new/usr/mdec/boot new/boot
-cp /boot.cfg new/boot.cfg
-cp /etc/resolv.conf new/etc/resolv.conf
+' > $MNT/etc/fstab
+cp $MNT/usr/mdec/boot $MNT/boot
+cp /boot.cfg $MNT/boot.cfg
+cp /etc/resolv.conf $MNT/etc/resolv.conf
 
+echo 'PKG_PATH=http://cdn.netbsd.org/pub/pkgsrc/packages/NetBSD/x86_64/8.0/All/
+export PKG_PATH' >> $MNT/etc/profile
 
-( cd new/dev ; ./MAKEDEV all )
+( cd $MNT/dev ; ./MAKEDEV all )
 PKG_PATH=http://cdn.netbsd.org/pub/pkgsrc/packages/NetBSD/x86_64/8.0/All/
 export PKG_PATH
 
-curl -L https://github.com/goneri/cloud-init/archive/netbsd.tar.gz | tar xfz - -C new/tmp
+curl -L -k https://github.com/goneri/cloud-init/archive/netbsd.tar.gz | tar xfz - -C $MNT/tmp
 
 chroot new sh -c 'cd /tmp/cloud-init-netbsd; ./tools/build-on-netbsd'
 
 umount ${MNT}
 
 gpt biosboot -L root ${VND} 
-installboot -v -o timeout=1 /dev/rdk1 /usr/mdec/bootxx_ffsv2
+installboot -v -o timeout=0 /dev/r${dk_dev} /usr/mdec/bootxx_ffsv2
 vnconfig -u vnd0
+done
