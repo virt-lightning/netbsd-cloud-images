e7f33d0b0db62414f2c0dd5daecdc1c15ba2d5f1
Gonéri Le Bouder <goneri@lebouder.net>
2024-11-05 10:39:05 -0500

    pip with no cache directory

--- build.sh
+++ build.sh
@@ -146,7 +146,7 @@ curl -L -k https://github.com/${repo}/archive/${ref}.tar.gz | tar xfz - -C $MNT/
 
 pkg_add -P $MNT mozilla-rootcerts
 chroot $MNT /bin/sh -c 'mozilla-rootcerts install'
-chroot $MNT /bin/sh -c '. /etc/profile; pkg_add python312 py312-pip; python3.12 -m pip install jsonpointer'
+chroot $MNT /bin/sh -c '. /etc/profile; pkg_add python312 py312-pip; python3.12 -m pip --no-cache-dir install jsonpointer'
 chroot $MNT /bin/sh -c '. /etc/profile; cd /tmp/cloud-init-*; PYTHON=/usr/pkg/bin/python3.12 ./tools/build-on-netbsd'
 # Ensure we've got all the dependency installed as expected
 chroot $MNT /bin/sh -c '. /etc/profile; /usr/pkg/bin/python3.12 -mcloudinit.handlers.cloud_config'
